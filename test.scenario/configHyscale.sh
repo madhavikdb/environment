1e6516e32ecdc752626f8b62c1d5f3c6119b4f3e
madhavikdb <43172017+madhavikdb@users.noreply.github.com>
2020-06-02 02:07:51 +0530

    Update configHyscale.sh

--- test.scenario/configHyscale.sh
+++ test.scenario/configHyscale.sh
@@ -1,4 +1,4 @@
 mkdir -p .kube; cd .kube/
-echo -e  "#// copy cluster kubeconfig contents here /\n #// use the command line to execute commands " > /home/scrapbook/.kube/config
+echo -e  "#// copy cluster kubeconfig contents here \n #// use the command line to execute commands " >> /home/scrapbook/.kube/config
 curl -sSL https://get.hyscale.io | bash
 
